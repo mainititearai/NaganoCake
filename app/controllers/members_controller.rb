3d1c195f61a413eab6937032a6d22bbce3c05769
yutotanigawa <61495631+yutotanigawa@users.noreply.github.com>
2020-04-17 13:10:22 +0900

    [Add]ユーザー編集機能と編集ページの作成

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -5,9 +5,16 @@ class MembersController < ApplicationController
 	end
 
 	def edit
+		@member = current_member
 	end
 
 	def update
+		@member = current_member
+		if @member.update(member_params)
+			redirect_to member_path
+		else
+			render 'member_path'
+		end
 	end
 
 	def cancel
@@ -15,5 +22,10 @@ class MembersController < ApplicationController
 
 	def withdraw
 	end
+
+	  private
+	  def member_params
+ 	 	params.require(:member).permit(:email,:last_name,:first_name,:last_name_kana,:first_name_kana,:address,:postcode,:phone_number,:valid_status)
+	  end
 
 end
