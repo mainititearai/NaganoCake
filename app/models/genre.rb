86461f3d39992245fee7014f462dd493af869ff4
tatsuya828828 <kkjy011894@gmail.com>
2020-04-18 10:25:38 +0000

    商品一覧ページの件数を修正しました

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,10 +2,10 @@ class Genre < ApplicationRecord
 	has_many :products
 	validates :name, presence: true
 	validates :valid_status, presence: true
-	enum valid_status:{active: 0,is_deleted:1}
+	enum valid_status:{active: 0,is_deleted: 1}
 
 	def update!
-		if actice?
+		if active?
 			is_deleted!
 		else
 			active
