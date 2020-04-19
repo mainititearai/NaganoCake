e35456e14b212f3b3e6d37029097f16d7508bb12
tatsuya828828 <kkjy011894@gmail.com>
2020-04-19 10:23:10 +0000

    注文機能を追加しました

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,23 +11,24 @@ class OrdersController < ApplicationController
 	end
 
 	def confirm
-		  @order = Order.new
-		  @order.payment_method = params[:order_info][:payment_method]
+		@order = Order.new
+		@order.payment_method = params[:order_info][:payment_method]
+		#自分の住所
 		if params[:order_info][:address_kind] == "self_address"
-		  @order.address = current_member.address
-		  @order.postcode = current_member.postcode
-		  @order.name = current_member.name
+		@order.address = current_member.address
+		@order.postcode = current_member.postcode
+		@order.name = current_member.last_name+current_member.first_name
 		elsif params[:order_info][:address_kind] == "other_address"
-		  @shipping_address = ShippingAddress.find(params[:order_info][:other_address_id])
-		  @order.address = @shipping_address.address
-		  @order.postcode = @shipping_address.postcode
-		  @order.name = @shipping_address.name
+		@shipping_address = ShippingAddress.find(params[:order_info][:other_address_id])
+		@order.address = @shipping_address.address
+		@order.postcode = @shipping_address.postcode
+		@order.name = @shipping_address.name
 		elsif params[:order_info][:address_kind] == "new_address"
-		  @order.address = params[:order_info][:new_address]
-		  @order.postcode = params[:order_info][:new_postcode]
-		  @order.name = params[:order_info][:new_name]
+		@order.address = params[:order_info][:new_address]
+		@order.postcode = params[:order_info][:new_postcode]
+		@order.name = params[:order_info][:new_name]
 		end
-		  redirect_to orders_confirm_member_path
+		@cart_items = current_member.cart_items.all
 	end
 
 	def create
