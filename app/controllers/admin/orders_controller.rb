a3a0c85557a8d839b84fe02b9ebe44f36f1291d9
yutotanigawa <61495631+yutotanigawa@users.noreply.github.com>
2020-04-20 18:46:07 +0900

    [Add]注文詳細の画面設計完了

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,13 +3,16 @@ class Admin::OrdersController < ApplicationController
 
 	def index
 		@orders = Order.all.reverse_order
-		@order_items = OrderItem.all.reverse_order
 	end
 
 	def show
+		@order = Order.find(params[:id])
+		@order_items = @order.order_items.all
+		@order_item = OrderItem.find(params[:id])
 	end
 
 	def update
+		@cart_item = CartItem.find(params[:id])
 	end
 
 	def item_update
